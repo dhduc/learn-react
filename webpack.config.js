09f3b32e2570cf05213b06af54e0ca6a583deedd
Duc Dao <ducdh1@smartosc.com>
2017-07-08 10:45:00 +0700

    Update bower components

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 var path = require('path');
- var webpack = require('webpack');
+var webpack = require('webpack');
 
- module.exports = {
-     entry: './js/main.js',
-     output: {
-         path: path.resolve(__dirname, 'build'),
-         filename: 'main.bundle.js'
-     },
-     module: {
-         loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel-loader',
-				query: {presets: ['es2015', 'react']},
-			}
-		]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
- };
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|components)/,
+                loader: 'babel-loader',
+                query: {presets: ['es2015', 'react']},
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
